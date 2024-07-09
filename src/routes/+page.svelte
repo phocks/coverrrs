116c66d282e9fd4aa27f4cdbd2f9f9a2495fb9d7
Joshua Byrd <phocks@gmail.com>
2024-07-09 23:06:26 +1000

    Add another and horizontal scroll as well

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,8 +9,6 @@
   console.log(getNanoId());
 
   export let data: PageData;
-
-  $: console.log(data.feed);
 </script>
 
 <svelte:head>
@@ -19,32 +17,42 @@
 </svelte:head>
 
 <section>
-  <!-- <img
-		alt="Cover of 'When I Sing Mountains Dance' book wth lots of plants around it."
-		src="https://img.coverrrs.com/v1/images/testing/217a3075-61ff-488d-a5fb-3377192da0bf"
-		width="500"
-	/> -->
-
-  {#each data.feed as cover}
-    <img
-      alt={cover.title}
-      src="https://img.coverrrs.com/v1/images/testing/{cover.imageId}"
-      width="500"
-    />
-  {/each}
+  <div class="cover-container">
+    {#each data.feed as cover}
+      <div class="cover">
+        <img
+          alt={cover.title}
+          src="https://img.coverrrs.com/v1/images/coverrrs/{cover.imageId}"
+          width="500"
+        />
+      </div>
+    {/each}
+  </div>
 </section>
 
 <style lang="scss">
   section {
-    // display: flex;
-    // flex-direction: column;
-    // justify-content: center;
-    // align-items: center;
-    // flex: 1;
+    /* display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 1; */
   }
 
-  img {
-    // border-radius: 32px;
-    // max-width: 100%;
+  .cover-container {
+    /* disp lay: flex; */
+    overflow-x: auto; /* Enable horizontal scrolling */
+    white-space: nowrap; /* Prevent wrapping */
+  }
+
+  .cover {
+    display: inline-block;
+
+    img {
+      // border-radius: 32px;
+      // max-width: 100%;
+      max-height: 700px;
+      width: auto;
+    }
   }
 </style>
