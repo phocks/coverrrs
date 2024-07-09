2d8f0a9680daf48b0cd399c9873b99f24ed08ba3
Joshua Byrd <phocks@gmail.com>
2024-07-09 18:58:17 +1000

    Cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,50 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+  import type { PageData } from "./$types";
 
-	import { customAlphabet } from 'nanoid';
-	import { nolookalikesSafe } from 'nanoid-dictionary';
+  import { customAlphabet } from "nanoid";
+  import { nolookalikesSafe } from "nanoid-dictionary";
 
-	const getNanoId = customAlphabet(nolookalikesSafe, 21);
+  const getNanoId = customAlphabet(nolookalikesSafe, 21);
 
-	console.log(getNanoId());
+  console.log(getNanoId());
 
-	export let data: PageData;
+  export let data: PageData;
 
-	$: console.log(data.feed);
+  $: console.log(data.feed);
 </script>
 
 <svelte:head>
-	<title>Coverrrs / Some interesting book covers etc.</title>
-	<meta name="description" content="Some interesting book covers." />
+  <title>Coverrrs / Some interesting book covers etc.</title>
+  <meta name="description" content="Some interesting book covers." />
 </svelte:head>
 
 <section>
-	<!-- <img
+  <!-- <img
 		alt="Cover of 'When I Sing Mountains Dance' book wth lots of plants around it."
 		src="https://img.coverrrs.com/v1/images/testing/217a3075-61ff-488d-a5fb-3377192da0bf"
 		width="500"
 	/> -->
 
-	{#each data.feed as cover}
-		<img
-			alt={cover.title}
-			src="https://img.coverrrs.com/v1/images/testing/{cover.imageId}"
-			width="500"
-		/>
-	{/each}
+  {#each data.feed as cover}
+    <img
+      alt={cover.title}
+      src="https://img.coverrrs.com/v1/images/testing/{cover.imageId}"
+      width="500"
+    />
+  {/each}
 </section>
 
 <style lang="scss">
-	section {
-		// display: flex;
-		// flex-direction: column;
-		// justify-content: center;
-		// align-items: center;
-		// flex: 1;
-	}
+  section {
+    // display: flex;
+    // flex-direction: column;
+    // justify-content: center;
+    // align-items: center;
+    // flex: 1;
+  }
 
-	img {
-		// border-radius: 32px;
-		// max-width: 100%;
-	}
+  img {
+    // border-radius: 32px;
+    // max-width: 100%;
+  }
 </style>
